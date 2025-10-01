1f9f620f4fb97568a9da38b8ce3dcbd27d36f414
PQuan2003 <147889621+PQuan2003@users.noreply.github.com>
2025-10-01 10:32:59 +0700

    fix database

---
+++ backend/models/Result.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const sequelize = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
+require("../models");
+
+module.exports = (sequelize, DataTypes) => {
+  class Result extends Model {}
+  Result.init(
+    {
+      result_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
+      },
+      preference_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      place_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Result",
+      tableName: "Results",
+      timestamps: true,
+    }
+  );
+
+  Result.associate = (models) => {
+    Result.belongsTo(models.User_Preference, {
+      foreignKey: "preference_id",
+      targetKey: "preference_id",
+      onDelete: "CASCADE",
+    });
+    Result.hasMany(models.Result_Location, {
+      foreignKey: "result_id",
+    });
+  };
+  module.exports = Result;
+  return Result;
+};
